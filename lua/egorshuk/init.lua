1746eda100e5b9b2578dad92d207ac33b0017f2d
egorshuk <egorrshuk@gmail.com>
2024-06-30 15:13:16 +0700

    adding everything

---
+++ lua/egorshuk/init.lua
@@ -0,0 +1,3 @@
+require("egorshuk.remap")
+require("egorshuk.set")
+require("egorshuk.lazy_init")
---
+++ lua/egorshuk/lazy/init.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"nvim-lua/plenary.nvim",
+		name = "plenary",
+		lazy = false,
+		priority = 1000
+	},
+	{
+		"shaunsingh/nord.nvim",
+		name = "nord",
+		config = function()
+			vim.cmd([[colorscheme nord]])
+            vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+            vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+		end
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		end
+	},
+    {
+        "theprimeagen/vim-be-good"
+    }
+}
