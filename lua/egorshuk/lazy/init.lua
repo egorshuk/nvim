e44cb2feb2943db51ab34d8b6097c33401201d80
egorshuk <egorrshuk@gmail.com>
2024-07-08 14:14:53 +0700

    adding neogit and removing fugitive

--- lua/egorshuk/lazy/init.lua
+++ lua/egorshuk/lazy/init.lua
@@ -20,12 +20,6 @@ return {
 			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 		end
 	},
-	{
-		"tpope/vim-fugitive",
-		config = function()
-			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		end
-	},
     {
         "theprimeagen/vim-be-good"
     }
